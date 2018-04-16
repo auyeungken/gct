2f53b81cc034aaba9c8279415fabd054a61e37ff
Ken Au Yeung <kenauyeung@hotmail.com>
2018-04-16 11:58:35 +0800

    update

--- contracts/token/GCToken.sol
+++ contracts/token/GCToken.sol
@@ -14,26 +14,4 @@ contract GCToken is PrivateSale {
     string constant public name = "GlobeCas";
     string constant public symbol = "GCT";
     uint8 constant public decimals = 8;
-
-     // ********************************** need to remove after testing **************************    
-    function testSetPrivateSaleReleaseTime(uint _v) public {
-        privateSaleReleaseTime = _v;
-    }
-
-    function testSetPrivateInvestLockToken(uint _v)public{
-        require(_v > 0);
-        privateInvestLockToken = _v;
-    }
-
-    function testGiveAccountSomeBal(address _to, uint _amount)public{
-        require(totalSupply_.add(_amount) <= cap);
-        totalSupply_ = totalSupply_.add(_amount);
-        balances[_to] = balances[_to].add(_amount);
-        emit Mint (_to, _amount);
-        emit Transfer(address(0), _to, _amount);
-    }
-    
-    function testSetIcoEndTime(uint _v)public{
-        icoEndTime = _v;
-    }
 }
